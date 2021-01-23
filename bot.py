6ccf0bdbfad06383f3df3fbb49606d85d35b46d9
RefinedDev <aryantatee@gmail.com>
2021-01-23 22:41:37 +0530

    'lol'

--- bot.py
+++ bot.py
@@ -154,10 +154,15 @@ async def on_member_remove(member):
     except Exception as e:
         print(f"An error occured on_member_remove {e}")
 
-@client.event
-async def on_reaction_add(reaction,user):
-    print(user)
-    print(reaction)
+@commands.Cog.listener()
+async def on_raw_reaction_add(self, payload):
+    channel = await client.fetch_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    user = await client.fetch_user(payload.user_id)
+    emoji = payload.emoji
+
+    await channel.send(emoji + str(channel.id) + user.name)
+
 
 @client.listen('on_message')
 async def botping(message):
