c99ff7f7125562a59c72206f24d5e96ac57f2b5e
RefinedDev <aryantatee@gmail.com>
2021-01-27 14:54:02 +0530

    l

--- cogs/highrank.py
+++ cogs/highrank.py
@@ -3,6 +3,7 @@ from discord.ext import commands,tasks
 from discord.ext.commands import Cog
 from datetime import datetime
 import mysql.connector
+import asyncio
 
 
 class HighRank(Cog):
@@ -36,20 +37,31 @@ class HighRank(Cog):
     #ClearChat
     @commands.command(aliases=['clear'])
     @commands.has_permissions(kick_members = True)
-    async def purge(self,ctx,amount : int):
-        await ctx.channel.purge(limit = amount)
-
-    @purge.error
-    async def clear_error(self,ctx,error):
-        if isinstance(error,commands.MissingRequiredArgument):
+    async def purge(self,ctx,amount : int = None):
+        if amount == None:
             embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
             embeddd.add_field(name = "Missing Number",value = "Please specify a Integer of how many messages you want to purge.",inline= False)
             embeddd.add_field(name = "Command Example",value = "`?purge 100`",inline= False)
             await ctx.send(embed = embeddd,delete_after=5)
+            return
+
+        await ctx.channel.purge(limit = amount)
 
     @commands.command()
     @commands.has_permissions(kick_members = True)
-    async def announce(self,ctx,channelid,*,text):
+    async def announce(self,ctx,channelid = None,*,text = None):
+        if channelid == None:
+            embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
+            embeddd.add_field(name = "Missing ChannelId",value = "Please specify channelid.",inline= False)
+            embeddd.add_field(name = "Command Example",value = "`?announce 4209409582859589 Hello everyone`",inline= False)
+            await ctx.send(embed = embeddd,delete_after=5)
+            return
+        if text == None:
+            embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
+            embeddd.add_field(name = "Missing Text",value = "Please specify the text.",inline= False)
+            embeddd.add_field(name = "Command Example",value = "`?announce 4209409582859589 Hello everyone`",inline= False)
+            await ctx.send(embed = embeddd,delete_after=5)
+            return
         channel = self.client.get_channel(int(channelid))
         if channel != None:
             embed = discord.Embed()
@@ -58,28 +70,27 @@ class HighRank(Cog):
         else:
             await ctx.send("Channel Not Found.")
 
-    @announce.error
-    async def announce_error(self,ctx,error):
-        if isinstance(error,commands.MissingRequiredArgument):
-            embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
-            embeddd.add_field(name = "Missing Text Or ChannelId",value = "Please specify the text and the channelid.",inline= False)
-            embeddd.add_field(name = "Command Example",value = "`?announce 4209409582859589 Hello everyone`",inline= False)
-            await ctx.send(embed = embeddd,delete_after=5)
+
 
     @commands.command()
     @commands.has_permissions(kick_members = True)
-    async def slowmode(self,ctx, seconds: int):
+    async def slowmode(self,ctx, seconds: int = None):
+        if seconds == None:
+            await ctx.send(f'This channel has a slowmode of {ctx.channel.slowmode_delay} seconds.')
+            return
         await ctx.channel.edit(slowmode_delay=seconds)
         await ctx.send(f"Slowmode Changed To {seconds}!")
 
-    @slowmode.error
-    async def slowmo_error(self,ctx,error):
-        if isinstance(error,commands.MissingRequiredArgument):
-            await ctx.send(f'This channel has a slowmode of {ctx.channel.slowmode_delay} seconds.')
-
     @commands.command()
     @commands.has_permissions(kick_members = True)
-    async def warn(self,ctx,user : discord.Member,*,reason = 'Not Specified'):
+    async def warn(self,ctx,user : discord.Member = None,*,reason = 'Not Specified'):
+        if user == None:
+            embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
+            embeddd.add_field(name = "Missing User Or Reason",value = "Specify the user and reason pal.",inline= False)
+            embeddd.add_field(name = "Command Example",value = "`?warn 1938194824019 idk cuh`",inline= False)
+            await ctx.send(embed = embeddd,delete_after=5)
+            return
+
         db = mysql.connector.connect(
             host = "us-cdbr-east-02.cleardb.com",
             user = "bc4de25d94d683",
@@ -97,18 +108,17 @@ class HighRank(Cog):
         await user.send(f'You were warned in {ctx.guild.name} reason: {str(reason)}')
         cursor.close()
         db.close()
-
-    @warn.error
-    async def warn_error(self,ctx,error):
-        if isinstance(error,commands.MissingRequiredArgument):
-            embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
-            embeddd.add_field(name = "Missing User Or Reason",value = "Specify the user and reason pal.",inline= False)
-            embeddd.add_field(name = "Command Example",value = "`?warn 1938194824019 idk cuh`",inline= False)
-            await ctx.send(embed = embeddd,delete_after=5)
     
     @commands.command(aliases = ['infraction','modlogs'])
     @commands.has_permissions(kick_members = True)
-    async def inf(self,ctx,user : discord.Member):
+    async def inf(self,ctx,user : discord.Member = None):
+        if user == None:
+            embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
+            embeddd.add_field(name = "Missing User",value = "Specify the user pal.",inline= False)
+            embeddd.add_field(name = "Command Example",value = "`?inf 1938194824019 `",inline= False)
+            await ctx.send(embed = embeddd,delete_after=5)
+            return
+
         db = mysql.connector.connect(
             host = "us-cdbr-east-02.cleardb.com",
             user = "bc4de25d94d683",
@@ -130,19 +140,19 @@ class HighRank(Cog):
         cursor.close()
         db.close()
 
-    @inf.error
-    async def inf_error(self,ctx,error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
-            embeddd.add_field(name = "Missing User",value = "Specify the user pal.",inline= False)
-            embeddd.add_field(name = "Command Example",value = "`?inf 1938194824019 `",inline= False)
-            await ctx.send(embed = embeddd,delete_after=5)
-
     
     @commands.command()
     @commands.cooldown(1,60,commands.BucketType.user)
     @commands.has_permissions(kick_members = True)
-    async def revoke_inf(self,ctx,infid):
+    async def revoke_inf(self,ctx,infid = None):
+        if infid == None:
+            embeddd = discord.Embed(colour= discord.Colour.red())
+            embeddd.add_field(name = "Missing InfractionID",value = "Specify the infractionID pal.",inline= False)
+            embeddd.add_field(name = "Command Example",value = "`?warn_revoke 1`",inline= False)
+            embeddd.set_footer(text  = "PS: To get the ID of the infraction run the ?inf (userID) command and get the id of infraction.")
+            await ctx.send(embed = embeddd,delete_after=5)
+            return
+
         db = mysql.connector.connect(
             host = "us-cdbr-east-02.cleardb.com",
             user = "bc4de25d94d683",
@@ -161,15 +171,6 @@ class HighRank(Cog):
             await ctx.send(f'Warn with id {infid} was revoked.')
         cursor.close()
         db.close()
-
-    @revoke_inf.error
-    async def warn_reveke(self,ctx,error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            embeddd = discord.Embed(colour= discord.Colour.red())
-            embeddd.add_field(name = "Missing InfractionID",value = "Specify the infractionID pal.",inline= False)
-            embeddd.add_field(name = "Command Example",value = "`?warn_revoke 1`",inline= False)
-            embeddd.set_footer(text  = "PS: To get the ID of the infraction run the ?inf (userID) command and get the id of infraction.")
-            await ctx.send(embed = embeddd,delete_after=5)
 
     @commands.command()
     @commands.has_permissions(kick_members = True)
@@ -200,7 +201,7 @@ class HighRank(Cog):
 
 
     @commands.command()
-    @commands.has_permissions(kick_members = True)
+    @commands.has_permissions(ban_members = True)
     async def ban(self,ctx,member : discord.Member = None,*, reason = 'Not Specified'):
         if member == None:
             embeddd = discord.Embed(colour= discord.Colour.red())
@@ -226,6 +227,115 @@ class HighRank(Cog):
             cursor.close() 
             db.close()
 
+    @commands.command()
+    @commands.cooldown(1,60,commands.BucketType.user)
+    @commands.has_permissions(kick_members = True)
+    async def mute(self,ctx,user : discord.Member = None,time = None,*,reason = 'Not Specified'):
+        hourandminute = {
+            "s": "1",
+            "hr": "3600",
+            "min": "60",
+            "h": "3600",
+            "m": "60"
+        }
+        if user == None:
+            embeddd = discord.Embed(colour= discord.Colour.red())
+            embeddd.add_field(name = "Missing User",value = "Specify the user pal.",inline= False)
+            embeddd.add_field(name = "Command Example",value = "`?mute 4289358298 10hr idk cuh`",inline= False)
+            await ctx.send(embed = embeddd,delete_after=5)
+            return
+
+        if time == None:
+            embeddd = discord.Embed(colour= discord.Colour.red())
+            embeddd.add_field(name = "Missing Duration",value = "Specify the duration pal.",inline= False)
+            embeddd.add_field(name = "Command Example",value = "`?mute 4289358298 10hr idk cuh`",inline= False)
+            await ctx.send(embed = embeddd,delete_after=5)
+            return
+
+        role = discord.utils.get(ctx.guild.roles, name="Muted")
+
+        if not role:
+            await ctx.send("Hey bro, there is no role called muted in your server. Create one called. `Muted`")
+            return
+        
+        if role in user.roles:
+            await ctx.send('Person is already muted.')
+            return
+
+        db = mysql.connector.connect(
+                host = "us-cdbr-east-02.cleardb.com",
+                user = "bc4de25d94d683",
+                passwd = "0bf00100",
+                database = "heroku_1d7c0ca78dfc2ef"
+        )
+
+        cursor = db.cursor()
+        guildid = str(ctx.guild.id)
+
+        cursor.execute('SELECT userid FROM mutedata WHERE userid = ' + str(user.id))
+        res = cursor.fetchall()
+        if (len(res) == 0):
+            for i in hourandminute:
+                split = time.split(i)
+                if split[0] != time:
+                    try:
+                        newtime = int(split[0])
+                        newtime = newtime * int(hourandminute[i])
+                        sql = "INSERT INTO logs (userid,reason,guildid,type) VALUES (%s, %s, %s, %s)"
+                        val = (str(user.id),str(reason),guildid,'Mute')
+                        cursor.execute(sql,val)
+                        db.commit()
+                        sql = "INSERT INTO mutedata (userid,guildid,time) VALUES (%s, %s, %s, %s)"
+                        val = (str(user.id),guildid,str(newtime))
+                        cursor.execute(sql,val)
+                        db.commit()
+                        await user.add_roles(role)
+                        await ctx.send(f'Muted {user.mention} for {str(newtime)} seconds: {str(reason)}')
+                        if newtime < 300:
+                            asyncio.sleep(newtime)
+                            await user.remove_roles(role)
+                            print(f'{user.name} has been unmuted.')
+                    except Exception as e:
+                        await ctx.send(f'An Error Occured: {e}')
+        else:
+            await ctx.send('My database says that the person is already muted.')
+        cursor.close()
+        db.close()
+
+
+    @commands.command()
+    @commands.has_permissions(kick_members = True)
+    @commands.cooldown(1,60,commands.BucketType.user)
+    async def unmute(self,ctx,user : discord.Member = None):
+        if user == None:
+            embeddd = discord.Embed(colour= discord.Colour.red())
+            embeddd.add_field(name = "Missing User",value = "Specify the user pal.",inline= False)
+            embeddd.add_field(name = "Command Example",value = "`?mute 4289358298 10hr idk cuh`",inline= False)
+            await ctx.send(embed = embeddd,delete_after=5)
+            return
+        role = discord.utils.get(ctx.guild.roles, name="Muted")
+        if not role:
+            await ctx.send("Hey bro, there is no role called muted in your server. Create one called. `Muted`")
+            return
+        db = mysql.connector.connect(
+                host = "us-cdbr-east-02.cleardb.com",
+                user = "bc4de25d94d683",
+                passwd = "0bf00100",
+                database = "heroku_1d7c0ca78dfc2ef"
+        )
+
+        cursor = db.cursor()
+        cursor.execute('SELECT userid FROM mutedata WHERE userid = ' + str(user.id))
+        res = cursor.fetchall()
+        if len(res) == 0:
+            await ctx.send('Person is not muted bro.')
+            return
+        else:
+            cursor.execute('DELETE FROM mutedata WHERE userid = ' + str(user.id))
+            await user.remove_roles(role)
+            await ctx.send(f'Successfully unmuted {user.mention}!')
+
+        
 
 
 
