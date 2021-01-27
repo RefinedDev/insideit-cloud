e48e38ff9b235601cf7cd18967112091f032969a
RefinedDev <aryantatee@gmail.com>
2021-01-27 08:43:05 +0530

    lol

--- bot.py
+++ bot.py
@@ -164,48 +164,53 @@ async def botping(message):
 @client.listen('on_message')
 async def nolink(message):
     if 'discord.gg' in message.content:
-        db = mysql.connector.connect(
-            host = "us-cdbr-east-02.cleardb.com",
-            user = "bc4de25d94d683",
-            passwd = "0bf00100",
-            database = "heroku_1d7c0ca78dfc2ef"
-        )
+        try:
+            db = mysql.connector.connect(
+                host = "us-cdbr-east-02.cleardb.com",
+                user = "bc4de25d94d683",
+                passwd = "0bf00100",
+                database = "heroku_1d7c0ca78dfc2ef"
+            )
 
-        cursor = db.cursor()
-        cursor.execute('SELECT toggle,discordlink FROM antilink WHERE guildid = ' + str(message.guild.id))
-        res = cursor.fetchall()
-        if (len(res) == 0):
-            return
-        if res[0][0] == "ON":
-            if res[0][1] == 'YES':
-                await message.delete()
-        else:
-            pass
-        cursor.close()
-        db.close()
-    
+            cursor = db.cursor()
+            cursor.execute('SELECT toggle,discordlink FROM antilink WHERE guildid = ' + str(message.guild.id))
+            res = cursor.fetchall()
+            if (len(res) == 0):
+                return
+            if res[0][0] == "ON":
+                if res[0][1] == 'YES':
+                    await message.delete()
+            else:
+                pass
+            cursor.close()
+            db.close()
+        except Exception as e:
+            print(f'An error occured in no link {e}')
+        
     if 'https://' in message.content or 'http://' in message.content:
-        db = mysql.connector.connect(
-            host = "us-cdbr-east-02.cleardb.com",
-            user = "bc4de25d94d683",
-            passwd = "0bf00100",
-            database = "heroku_1d7c0ca78dfc2ef"
-        )
+        try:
+            db = mysql.connector.connect(
+                host = "us-cdbr-east-02.cleardb.com",
+                user = "bc4de25d94d683",
+                passwd = "0bf00100",
+                database = "heroku_1d7c0ca78dfc2ef"
+            )
 
-        cursor = db.cursor()
-        cursor.execute('SELECT toggle,discordlink FROM antilink WHERE guildid = ' + str(message.guild.id))
-        res = cursor.fetchall()
-        if (len(res) == 0):
-            return
+            cursor = db.cursor()
+            cursor.execute('SELECT toggle,discordlink FROM antilink WHERE guildid = ' + str(message.guild.id))
+            res = cursor.fetchall()
+            if (len(res) == 0):
+                return
 
-        if res[0][0] == "ON":
-            if res[0][1] == 'YES':
-                await message.delete()
-        else:
-            pass
-        cursor.close()
-        db.close()
-
+            if res[0][0] == "ON":
+                if res[0][1] == 'YES':
+                    await message.delete()
+            else:
+                pass
+            cursor.close()
+            db.close()
+        except Exception as e:
+            print(f'An error occured in no link {e}')
 
 cogslist = ['cogs.misc','cogs.help','cogs.APIscrape','cogs.RPS','cogs.configurations','cogs.highrank','cogs.img']
 
