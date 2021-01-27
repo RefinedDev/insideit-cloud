19a52e03e0c85a3416be0b0850a8b14b37dc48fc
RefinedDev <aryantatee@gmail.com>
2021-01-27 17:09:50 +0530

    lol

--- cogs/highrank.py
+++ cogs/highrank.py
@@ -363,7 +363,7 @@ class HighRank(Cog):
 
             for i in res:
                 print(i)
-                time = datetime.strftime(i[3],"%m/%d/%Y, %H:%M:%S GMT")
+                time = datetime.strptime(i[3],"%m/%d/%Y, %H:%M:%S GMT")
                 unmuteTime = time + relativedelta(seconds= i[1])
                 if currentime >= unmuteTime:
                     guild = self.client.get_guild(int(i[2]))
