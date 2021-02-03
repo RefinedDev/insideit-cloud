8bccb3377321278768a9652d17fe0f8770f3054f
RefinedDev <aryantatee@gmail.com>
2021-02-03 15:27:03 +0530

    l

--- cogs/tags.py
+++ cogs/tags.py
@@ -35,7 +35,7 @@ class Tags(Cog):
 
     @commands.group()
     async def tag(self,ctx,name = None):
-        if name == None:
+        if name == None and ctx.invoked_subcommand is None:
             embed = discord.Embed(title = 'TAGS',description = 'Shows a content of tag is it exists.',color = ctx.author.color)
             embed.add_field(name = 'Commands',value = "`?tag create: Create a tag`\n`?tag remove (tag): Delete a tag`\n`?tag edit (tag): Edit a tag's content`\n`?tag (tag): Reveal a tag's content aka use it.`\n`?tag show: Show existing tags in the guild.`")
             await ctx.send(embed = embed)
