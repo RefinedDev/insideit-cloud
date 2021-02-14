4dd8c3889292936184270c84ac224d626232d285
RefinedDev <aryantatee@gmail.com>
2021-02-14 13:30:12 +0530

    l

--- bot.py
+++ bot.py
@@ -228,7 +228,7 @@ async def nolink(message):
         except Exception as e:
             print(f'An error occured in no link {e}')
 
-cogslist = ['cogs.misc','cogs.help','cogs.APIscrape','cogs.RPS','cogs.configurations','cogs.highrank','cogs.img','cogs.tags','cogs.music']
+cogslist = ['cogs.misc','cogs.help','cogs.APIscrape','cogs.RPS','cogs.configurations','cogs.highrank','cogs.img','cogs.tags']
 
 if __name__ == '__main__':
     for i in cogslist:
