b7ca96ecf660a5cc2b317626d4096ec7f03da98c
RefinedDev <aryantatee@gmail.com>
2021-02-19 12:51:30 +0530

    l

--- cogs/configurations.py
+++ cogs/configurations.py
@@ -573,7 +573,7 @@ class config(Cog):
                             emoji2 = staticemoji.encode(encoding = 'utf_7')
                             emoji = emoji2.decode('utf-8')
                             cursor.execute(f"SELECT roleid FROM reactionroles WHERE guildid = {str(ctx.guild.id)} AND channelid = {str(channel.id)} AND emoji = '{emoji}' AND messageid = {denymsg2.content}")
-                            print(cursor)
+                            await ctx.send(cursor)
                             res = cursor.fetchall()
                             if len(res) == 0:
                                 await ctx.send(f'Could not find the reaction role with emoji {staticemoji}')
