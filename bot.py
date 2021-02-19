8d005861c2b6f250a92616ce38e22c7095861535
RefinedDev <aryantatee@gmail.com>
2021-02-19 09:46:20 +0530

    l

--- bot.py
+++ bot.py
@@ -68,10 +68,11 @@ async def on_raw_reaction_add(payload):
         staticemoji = str(payload.emoji)
         channelid = payload.channel_id
         messageid = payload.message_id
-        emoji = staticemoji.encode(encoding = 'utf_7')
+        emoji2 = staticemoji.encode(encoding = 'utf_7')
+        emoji = emoji2.decode().split('b')[0]
         guildid = payload.guild_id
         member = payload.member
-        cursor.execute(f"SELECT roleid FROM reactionroles WHERE guildid = {str(guildid)} AND channelid = {str(channelid)} AND messageid = {(str(messageid))} AND emoji = {emoji}")
+        cursor.execute(f"SELECT roleid FROM reactionroles WHERE guildid = {str(guildid)} AND channelid = {str(channelid)} AND messageid = {(str(messageid))} AND emoji = '{emoji}'")
         res = cursor.fetchall()
         if len(res) == 0:
             return
@@ -107,29 +108,28 @@ async def on_raw_reaction_remove(payload):
         staticemoji = str(payload.emoji)
         channelid = payload.channel_id
         messageid = payload.message_id
-        emoji = staticemoji.encode(encoding = 'utf_7')
-        emoji2 = emoji.decode().split('b')
-        print(emoji2)
-        # guildid = payload.guild_id
-        # member = payload.member
-        # cursor.execute(f"SELECT roleid FROM reactionroles WHERE guildid = {str(guildid)} AND channelid = {str(channelid)} AND messageid = {(str(messageid))} AND emoji = '{emoji}'")
-        # res = cursor.fetchall()
-        # if len(res) == 0:
-        #     return
+        emoji2 = staticemoji.encode(encoding = 'utf_7')
+        emoji = emoji2.decode().split('b')[0]
+        guildid = payload.guild_id
+        member = payload.member
+        cursor.execute(f"SELECT roleid FROM reactionroles WHERE guildid = {str(guildid)} AND channelid = {str(channelid)} AND messageid = {(str(messageid))} AND emoji = '{emoji}'")
+        res = cursor.fetchall()
+        if len(res) == 0:
+            return
 
-        # guild = client.get_guild(int(guildid))
-        # if guild != None:
-        #     role = discord.Utils.get(guild.roles, id = res[0][0])
-        #     if role != None:
-        #         if role not in member.roles:
-        #             return
+        guild = client.get_guild(int(guildid))
+        if guild != None:
+            role = discord.Utils.get(guild.roles, id = res[0][0])
+            if role != None:
+                if role not in member.roles:
+                    return
                     
-        #         await member.remove_roles(role)
-        #         await member.send(f"You're {role.name} has been removed!")
-        #     else:
-        #         return
-        # else:
-        #     return
+                await member.remove_roles(role)
+                await member.send(f"You're {role.name} has been removed!")
+            else:
+                return
+        else:
+            return
 
     except Exception as e:
         print(f'An error occured in reactionrolesremove: {e}')
