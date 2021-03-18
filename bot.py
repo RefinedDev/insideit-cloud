dca5ad2d34eca6cb76b0214001d6fdfb233bbef1
RefinedDev <aryantatee@gmail.com>
2021-03-18 13:47:56 +0530

    l

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ async def on_command_error(ctx,error):
     elif isinstance(error,commands.MissingRequiredArgument):
         pass
     else:
-        embed = discord.embed()
+        embed = discord.Embed()
         embed.add_field(name = 'ERROR',value = f'{error}')
         await ctx.send(embed = embed)
 
