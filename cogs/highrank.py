106634a60729cdc25d9adf73e11e3a714fdba51f
RefinedDev <aryantatee@gmail.com>
2021-04-20 17:09:58 +0530

    l

--- cogs/highrank.py
+++ cogs/highrank.py
@@ -38,7 +38,7 @@ class HighRank(Cog):
 
     #ClearChat
     @commands.command(aliases=['clear'])
-    @commands.has_permissions(permissions['Purge'])
+    @commands.has_permissions(permissions['Purge'].strip("'"))
     async def purge(self,ctx,amount : int = None):
         if amount == None:
             embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
