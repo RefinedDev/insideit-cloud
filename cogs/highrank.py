db8344a8964ecbde994a6d43318c8240d8e1ee1c
RefinedDev <aryantatee@gmail.com>
2021-04-21 16:11:40 +0530

    l

--- cogs/highrank.py
+++ cogs/highrank.py
@@ -16,8 +16,9 @@ class HighRank(Cog):
         self.mute_loop.start()
     
     #ClearChat
+    dic = {"manage_messages = True": ''}
     @commands.command(aliases=['clear'])
-    @commands.has_permissions(manage_messages = True)
+    @commands.has_permissions(**dic)
     async def purge(self,ctx,amount : int = None):
         if amount == None:
             embeddd = discord.Embed(timestamp = datetime.utcnow(),colour= discord.Colour.red())
